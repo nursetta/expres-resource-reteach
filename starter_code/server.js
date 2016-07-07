03618b72bfd00c3b944c9796be84f6fa13131c49
nursetta <n_ursetta@yahoo.com>
2016-07-06 23:47:43 -0600

    stuck on post no id

--- starter_code/server.js
+++ starter_code/server.js
@@ -1,7 +1,8 @@
 'use strict'
-var express = require('express')
-var app = express() 
-
+var express = require('express');
+var app = express() ;
+var bodyParser = require('body-parser');
+var router = express.Router();
 var candies = [
   {
     "id": 1,
@@ -20,5 +21,38 @@ var candies = [
   }
 ];
 
-app.listen(3000)
-console.log("Server listening on port 3000")
+
+app.listen(3000);
+console.log("Server listening on port 3000");
+
+app.get('/candies', function (req, res) {
+  res.send(candies);
+});
+
+app.get('/candies/:id', function(req, res) {
+  var searchId = parseInt(req.params.id);
+    for(var i = 0; i< candies.length; i++) {
+      if (candies[i].id === searchId) {
+        res.send(candies[i]);
+      }
+    }
+});
+
+app.use(bodyParser.json());
+
+app.post('/candies', function (req, res) {
+  var newCandy = req.body;
+  console.log(newCandy);
+  candies.push(newCandy);
+  res.json(candies);
+  });
+
+app.put('/candies/:id', function (req, res) {
+  var id = req.body.id;
+  //for(var i = 0; i< candies.length; i++) {
+    if (candies.id === undefined) {
+      console.log(id);
+      candies.push(id);
+      res.json(candies);
+      }
+});
